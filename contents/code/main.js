1e78978ee2597bd4afc0d0275e42591c1fb9dbc2
Dorian OUAKLI <dorian.ouakli@xwiki.com>
2025-06-18 08:15:46 +0200

    WIP: Expand in 2D

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,310 +1,243 @@
-// This scripts relies on two things:
-// 1. `workspace.desktops` and `client.desktops` giving me desktops in the same
-//   order as in pager and all context menus
-// 2. Desktops being comparable for equality with `==` operator
-
-// Desktop numbers are from zero (unlike how it worked in plasma 5)
-
-
-const MIN_DESKTOPS = 2;
-const LOG_LEVEL = 2; // 0 trace, 1 debug, 2 info
+const LOG_LEVEL = 1; // 0 trace, 1 debug, 2 info
 
 
 function log(...args) { print("[dynamic_workspaces] ", ...args); }
 function debug(...args) { if (LOG_LEVEL <= 1)  log(...args); }
 function trace(...args) { if (LOG_LEVEL <= 0)  log(...args); }
 
-
-// In plasma 6 simply removing a desktop breaks the desktop switching
-// animation. We use a workaround with emitting more switches at the right time
-// to force the animation to play
-let animationFixup = false;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
-/*****  Plasma 5/6 differences  *****/
-
-
-const isKde6 = typeof workspace.windowList === "function";
-const compat = isKde6
-	?
-		{ addDesktop = () =>
-			{ workspace.createDesktop(workspace.desktops.length, undefined); }
-		, windowAddedSignal = ws => ws.windowAdded
-		, windowList = ws => ws.windowList()
-		, desktopChangedSignal = c => c.desktopsChanged
-
-		, toDesktop = d => d
-		, workspaceDesktops = () => workspace.desktops
-		, lastDesktop = () => workspace.desktops[workspace.desktops.length - 1]
-		, deleteLastDesktop = () =>
-			{
-				try
-				{
-					animationFixup = true;
-
-					const last = workspace.desktops[workspace.desktops.length - 1];
-					// replay the animation by switching again
-					const current = workspace.currentDesktop;
-					const index = workspace.desktops.indexOf(current);
-					// in any weird corner case switch to current desktop
-					const target = index + 1 < workspace.desktops.length || index === -1
-						? workspace.desktops[index + 1]
-						: current;
-
-					workspace.currentDesktop = target;
-					workspace.removeDesktop(last);
-					workspace.currentDesktop = current;
-				}
-				finally
-				{
-					animationFixup = false;
-				}
-			}
-		, findDesktop = (ds, d) => ds.indexOf(d)
-
-		, clientDesktops = c => c.desktops
-		, setClientDesktops = (c, ds) =>
-			{
-				c.desktops = ds;
-			}
-		, clientOnDesktop = (c, d) => c.desktops.indexOf(d) !== -1
-		}
-	:
-		{ addDesktop = () =>
-			{ workspace.createDesktop(workspace.desktops, "dyndesk"); }
-		, windowAddedSignal = ws => ws.clientAdded
-		, windowList = ws => ws.clientList()
-		, desktopChangedSignal = c => c.desktopChanged
-
-		// emulate plasma 6 behaviour with custom types
-		, toDesktop = number => {{ index: number - 1 }}
-		, workspaceDesktops = () =>
-			{
-				let r = [];
-				for (let i = 0; i < workspace.desktops; ++i)
-				{
-					const desktop =
-						{ index: i
-						};
-					r.push(desktop);
-				}
-				return r;
-			}
-		, lastDesktop = () => {{ index: workspace.desktops - 1 }}
-		, deleteLastDesktop = () =>
-			{ workspace.removeDesktop(workspace.desktops - 1); }
-		, findDesktop = (ds, d) =>
-			{
-				for (let i = 0; i < ds.length; ++i)
-				{
-					if (ds[i].index === d.index)
-					{
-						return i;
-					}
-				}
-				return -1;
-			}
-
-		, clientDesktops = c =>
-			c
-				.x11DesktopIds
-				.map(id => {return {index: id - 1}})
-		, setClientDesktops = (c, ds) =>
-			{
-				// Plasma 5 is supports window on multiple desktops, and there
-				// are even functions for it in the API, but they are bugged.
-				// So we have to do this. So far, noone has complained, so
-				// maybe noone uses this feature?
-				c.desktop = ds[0];
-			}
-		, clientOnDesktop = (c, d) => c.desktop === d.index + 1
-		};
-
-
-/*****  Logic definition  *****/
-
-
-// shifts a window to the left if it's more to the right than number
-function shiftRighterThan(client, number)
-{
-	trace(`shiftRighterThan(${client.caption}, ${number})`);
-	if (number === 0)  return;
-	// Build a new array by comparing old client desktops with all available
-	// desktops
-	const allDesktops = compat.workspaceDesktops();
-	const clientDesktops = compat.clientDesktops(client);
-	let newDesktops = [];
-	// we assume the desktop in both lists are sorted
-
-	let i = 0;
-	for (const d of clientDesktops)
-	{
-		// page through all desktops until we find the current one
-		while (d != allDesktops[i])
-		{
-			i += 1;
-			if (i > allDesktops.length)  throw new Error("Did the behaviour of equality change?");
-		};
-		// push either current, or the one before it
-		if (i < number || i === 0)
-		{
-			newDesktops.push(d);
-		}
-		else
-		{
-			newDesktops.push(allDesktops[i - 1]);
-		}
-	}
-
-	compat.setClientDesktops(client, newDesktops);
+function enumerate_obj(obj) {
+    for (let key in obj) {
+        console.log(key);
+    }
 }
 
 /**
- * Delete a desktop by index
+ * Adds one desktop at the end of the list.
  *
- * @returns true if desktop was deleted, false if wasn't
+ * To be used by addRow and addColumn.
+ *
+ * @param position optional
  */
-function removeDesktop(number)
-{
-	trace(`removeDesktop(${number})`);
+const _addDesktop = function (position, name) {
+    let target = position;
+    if (target === undefined) {
+        target = workspace.desktops.length;
+    }
+    workspace.createDesktop(target, name);
+};
 
-	const desktopsLength = compat.workspaceDesktops().length;
-	if (desktopsLength - 1 <= number)
-	{
-		debug("Not removing desktop at end");
-		return false;
-	}
-	if (desktopsLength <= MIN_DESKTOPS)
-	{
-		debug("Not removing desktop, too few left");
-		return false;
-	}
-
-	// plasma6 allows us to delete desktops in the middle. Unfortunately, this
-	// messes up pager, so we have to do what we did un plasma5 and shift all
-	// windows by hand to delete the last desktop
-	compat.windowList(workspace).forEach((client) =>
-	{
-		shiftRighterThan(client, number)
-	});
-	compat.deleteLastDesktop();
-	debug("Desktop removed");
-	return true;
-}
-
-// tells if desktop has no windows of its own
-function isEmptyDesktop(number)
-{
-	trace(`isEmptyDesktop(${number})`)
-	const desktop = compat.workspaceDesktops()[number];
-	const cls = compat.windowList(workspace);
-	for (client of cls)
-	{
-		if (compat.clientOnDesktop(client, desktop)
-			&& !client.skipPager // ignore hidden windows
-			&& !client.onAllDesktops // ignore windows on all desktops
-		) {
-			debug(`Desktop ${number} not empty because ${client.caption} is there`);
-			return false;
-		}
-	}
-
-	return true;
+/**
+ * Adds a given number of desktops.
+ *
+ * @param count the number of desktops to add.
+ */
+const _addDesktops = function(count) {
+    for (let i = 0; i < count; i++) {
+        _addDesktop();
+    }
 }
 
 /**
- * Checks for new created or moved windows if they are occupying the last desktop
- * -> if yes, create new one to the right
+ * Removes one desktop at the end of the list.
+ *
+ * Instead of removing other desktops, shift all the other ones to have an
+ * empty desktop at the end of the list.
+ *
+ * @param animate When true, animates the switch.
  */
-function onDesktopChangedFor(client)
-{
-	trace(`onDesktopChangedFor(${client.caption})`);
+const _deleteDesktop = function(animate) {
+    try {
+        const last = workspace.desktops[workspace.desktops.length - 1];
 
-	const lastDesktops = compat.lastDesktop();
-	if (compat.clientOnDesktop(client, lastDesktops))
-	{
-		compat.addDesktop();
-	}
+        // replay the animation by switching again
+        const current = workspace.currentDesktop;
+        const index = workspace.desktops.indexOf(current);
+
+        // in any weird corner case switch to current desktop
+        const target = index + 1 < workspace.desktops.length || index === -1
+            ? workspace.desktops[index + 1]
+            : current;
+
+        if (animate) {
+            workspace.currentDesktop = target;
+        }
+        workspace.removeDesktop(last);
+        if (animate) {
+            workspace.currentDesktop = current;
+        }
+    } finally {}
+};
+
+const _deleteDesktops = function(count, animate) {
+    for (let i = 0; i < count; i++) {
+        _deleteDesktop(animate)
+    }
+}
+
+
+/**
+ * Gets the list of windows on the given desktop.
+ *
+ * @param desktop the desktop to find windows for
+ * @return The list of windows for the given desktop
+ */
+const _windowsOfDesktop = function(desktop) {
+    const allWindows = workspace.windowList();
+    const windows = [];
+    for (let i = 0; i < allWindows.length; i++) {
+        const win = allWindows[i];
+
+        if (win.desktops.indexOf(desktop) !== -1) {
+            windows.push(win);
+        }
+    }
+
+    return windows;
+};
+
+/**
+ * Gets the list of desktops on a given row.
+ *
+ * @param the index of the row
+ * @return the list of desktops in the row
+ */
+const _getRow = function(index) {
+    const count = workspace.desktops.length;
+    const rows = workspace.desktopGridHeight;
+    const cols = workspace.desktopGridWidth;
+
+    const limit = max(count, (index+1)*cols);
+
+    const desktops = [];
+    for (let i=index*cols; i < limit ; i++) {
+        desktops.push(workspace.desktops[i]);
+    }
+
+    return desktops;
 }
 
 /**
- * When creating new windows, check whether they are occupying the last desktop
+ * Sends all windows from a desktop to another.
+ *
+ * @param sender the desktop to take the windows from
+ * @param recipient the desktop that will recieve the sender's windows
  */
-function onClientAdded(client)
-{
-	if (client === null)
-	{
-		log("onClientAdded(null) - that may happen rarely");
-		return;
-	}
-	trace(`onClientAdded(${client.caption})`);
+const _sendAllWindows = function(sender, recipient) {
+    const windows = _windowsOfDesktop(sender);
 
-	if (client.skipPager)
-	{
-		debug("Ignoring added hidden window");
-		return;
-	}
-
-	// add a new desktop for a client too right
-	if (compat.clientOnDesktop(client, compat.lastDesktop()))
-	{
-		compat.addDesktop();
-	}
-
-	// subscribe the client to create desktops when desktop switched
-	compat.desktopChangedSignal(client).connect(() => { onDesktopChangedFor(client); });
+    for (let i = 0; i < windows.length; i++) {
+        const win = windows[i];
+        const winDesktops = win.desktops.map(desktop => desktop == sender ? recipient : desktop);
+        win.desktops = winDesktops;
+    }
 }
 
 /**
- * Deletes empty desktops to the right in case of a left switch
+ * Shift clients across desktops in **1D** space towards the right.
+ *
+ * After calling this function:
+ *   Desktop at start will be empty.
+ *   For each i in [start,end], desktop at i+1 will have received the clients from desktop at i.
+ *   Note that the desktop at end+1 will receive clients.
+ *
+ * @param start index of the first desktop of the range
+ * @param end index of the last desktop of the range
  */
-function onDesktopSwitch(oldDesktop)
-{
-	trace(`onDesktopSwitch(${oldDesktop})`);
+const _shiftClientsRight = function(start, end, offset) {
 
-	if (animationFixup)  return;
+    if (offset) {
+        for (let i = 0; i<offset; i++) {
+            _shiftClientsRight(start+i, end+i);
+        }
+        return;
+    }
 
-	const allDesktops = compat.workspaceDesktops();
-	const oldDesktopIndex = compat.findDesktop(allDesktops, compat.toDesktop(oldDesktop));
-	const currentDesktopIndex = compat.findDesktop(allDesktops, compat.toDesktop(workspace.currentDesktop));
-	const getDesktopsLength = () => compat.workspaceDesktops().length;
-	const keepEmptyMiddleDesktops = readConfig("keepEmptyMiddleDesktops", false);
+    const desktops = workspace.desktops;
 
-	if (oldDesktopIndex <= currentDesktopIndex)
-	{
-		debug("Desktop switched to the right - ignoring");
-		return;
-	}
-
-	// Loop through desktops right-to-left and delete empty ones:
-	// - Starts from second-to-last desktop (preserving always one empty desktop at the end)
-	// - Stops before reaching current desktop (preserves what user is viewing)
-	// - Extra check (desktopIdx > 0) prevents an infinite loop caused by abnormal conditions
-	//   In case of interference with other plugins, we'll only examine as many desktops as we initially detect
-	for (let desktopIdx = getDesktopsLength() - 2; desktopIdx > currentDesktopIndex && desktopIdx > 0; --desktopIdx)
-	{
-		debug(`Examine desktop ${desktopIdx}`);
-		if (isEmptyDesktop(desktopIdx))
-		{
-			removeDesktop(desktopIdx);
-		}
-		else if (keepEmptyMiddleDesktops)
-		{
-			debug("Found non-empty desktop, stopping purge");
-			break;
-		}
-	}
+    for (let i = end; i >= start; i--) {
+        _sendAllWindows(desktops[i], desktops[i+1]);
+    }
 }
 
+/**
+ * Shifts clients across desktops in **1D** space towards the left.
+ *
+ * After calling this function:
+ *   Desktop at end will be empty.
+ *   For each i in [start, end], desktop at i-1 will have received the clients from desktop at i.
+ *   Note that the destop at start-1 will receive clients.
+ *
+ * @param start index of the first desktop of the range
+ * @param end index of the last desktop of the range
+ */
+const _shiftClientsLeft = function(start, end) {
 
-/*****  Main part *****/
+    if (offset) {
+        for (let i = 0; i<offset; i++) {
+            _shiftClientsLeft(start+i, end+i);
+        }
+        return;
+    }
 
+    const desktops = workspace.desktops;
 
-// Adding or removing a client might create desktops.
-// For all existing clients:
-compat.windowList(workspace).forEach(onClientAdded);
-// And for all future clients:
-compat.windowAddedSignal(workspace).connect(onClientAdded);
+    for (let i = start; i <= end; i++) {
+        _sendAllWindows(desktops[i], desktops[i-1]);
+    }
+}
 
-// Switching desktops might remove desktops
-workspace.currentDesktopChanged.connect(onDesktopSwitch);
+/**
+ * Adds a new empty row of desktops at the bottom of the grid.
+ */
+const appendRow = function() {
+    const rows = workspace.desktopGridHeight
+    const cols = workspace.desktopGridWidth
+    const count = workspace.desktops.length
+
+    _addDesktops(cols);
+
+    workspace.desktopGridHeight = rows + 1
+}
+
+/**
+ * Adds a new empty row of desktops at the top of the grid.
+ */
+const prependRow = function() {
+    const current = workspace.currentDesktop;
+    const index = workspace.desktops.indexOf(current);
+
+    const count = workspace.desktops.length;
+    const rows = workspace.desktopGridHeight;
+    const cols = workspace.desktopGridWidth;
+    appendRow();
+    _shiftClientsRight(0, count - 1, cols);
+    workspace.currentDesktop = workspace.desktops[index+cols]
+}
+
+// console.log(_addDesktops(20));
+// appendRow();
+// prependRow();
+// workspace.desktopGridHeight = 1;
+
+// enumerate_obj(workspace.windowList());
+// console.log("test");
+// _shiftClientsRight(8,12);
+// _shiftClientsLeft(9,13);
+
+/**
+ * Entrypoint.
+ */
+
+// // Adding or removing a client might create desktops.
+// // For all existing clients:
+// compat.windowList(workspace).forEach(onClientAdded);
+// // And for all future clients:
+// compat.windowAddedSignal(workspace).connect(onClientAdded);
+//
+// // Switching desktops might remove desktops
+// workspace.currentDesktopChanged.connect(onDesktopSwitch);
